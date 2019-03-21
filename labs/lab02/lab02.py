889d9d51f0e4629f83dc86dc7982919590da767b
fantasy <875282031@qq.com>
2019-03-21 14:36:40 +0800

    finish lab02

---
+++ labs/lab02/lab02.py
@@ -0,0 +1,26 @@
+"""Lab 2: Lambda Expressions and Higher Order Functions"""
+
+# Lambda Functions
+
+def lambda_curry2(func):
+    """
+    Returns a Curried version of a two-argument function FUNC.
+    >>> from operator import add
+    >>> curried_add = lambda_curry2(add)
+    >>> add_three = curried_add(3)
+    >>> add_three(5)
+    8
+    """
+    "*** YOUR CODE HERE ***"
+    return lambda x: lambda y: func(x, y)
+
+
+# x = 1
+# def f(x):
+#     x += 1
+#     return x
+#
+# a, b = f(x), x
+# print(x)   python 基本类型是值传递
+
+
